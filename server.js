39d8d832517e9268064367f421505102fde88f95
dchigu <dchigurupall2016@fau.edu>
2016-11-26 19:22:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,55 +1,3 @@
-var express = require('express');    //Express Web Server 
-var busboy = require('connect-busboy'); //middleware for form/file upload
-var path = require('path');     //used for file path
-var fs = require('fs-extra');       //File System - for file manipulation
-
-var app = express();
-app.use(busboy());
-app.use(express.static(path.join(__dirname, 'public')));
-
-/* ========================================================== 
-Create a Route (/upload) to handle the Form submission 
-(handle POST requests to /upload)
-Express v4  Route definition
-============================================================ */
-app.route('/upload')
-    .post(function (req, res, next) {
-
-        var fstream;
-        req.pipe(req.busboy);
-        req.busboy.on('file', function (fieldname, file, filename) {
-            console.log("Uploading: " + filename);
-
-            //Path where image will be uploaded
-            fstream = fs.createWriteStream(__dirname + '/img/' + filename);
-            file.pipe(fstream);
-            fstream.on('close', function () {    
-                console.log("Upload Finished of " + filename);              
-                res.redirect('back');           //where to go next
-            });
-        });
-    });
-
-var server = app.listen(3030, function() {
-    console.log('Listening on port %d', server.address().port);
-});
-INDEX.HTML
-
-<!DOCTYPE html>
-<html lang="en" ng-app="APP">
-<head>
-    <meta charset="UTF-8">
-    <title>angular file upload</title>
-</head>
-
-<body>
-        <form method='post' action='upload' enctype="multipart/form-data">
-        <input type='file' name='fileUploaded'>
-        <input type='submit'>
- </body>
-</html>
-The following will work with formidable SERVER.JS
-
 var express = require('express');   //Express Web Server 
 var bodyParser = require('body-parser'); //connects bodyParsing middleware
 var formidable = require('formidable');
@@ -62,8 +10,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 /* ========================================================== 
  bodyParser() required to allow Express to see the uploaded files
 ============================================================ */
-
-//upload file
 app.use(bodyParser({defer: true}));
  app.route('/upload')
  .post(function (req, res, next) {
@@ -95,7 +41,6 @@ app.use(bodyParser({defer: true}));
     });
 });
 
-// START THE SERVER
-// ==============================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+var server = app.listen(3030, function() {
+console.log('Listening on port %d', server.address().port);
+});
